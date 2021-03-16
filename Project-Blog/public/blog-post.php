989d99787fd4a58e554a31265079014f4ac5051b
huynguyen137 <30463880+huynguyen137@users.noreply.github.com>
2021-03-16 23:31:48 +0700

    Added HTML

---
+++ Project-Blog/public/blog-post.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bài viết</title>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="top-header">
+				<nav>
+					<ul>
+						<div>
+							<li><a href="">Mới nhất</a></li>
+						</div>
+						<div>
+							<li><a href="">Điều hướng</a></li>
+							<ul>
+								<li><a href="">Danh mục</a></li>
+								<li><a href="">Danh mục</a></li>
+								<li><a href="">Danh mục</a></li>
+								<li><a href="">Danh mục</a></li>
+							</ul>
+						</div>
+					</ul>
+				</nav>
+				<div class="logo">
+					<a href=""><img src="" alt="Logo"></a>
+				</div>
+				<div class="social">
+					<ul>
+						<div>
+							<li><a href="">Social</a></li>
+						</div>
+						<div>
+							<li><a href="">Social</a></li>
+						</div>
+						<div>
+							<li><a href="">Social</a></li>
+						</div>
+					</ul>
+				</div>
+			</div>
+		</header>
+	</div>
+</body>
+</html>
---
+++ Project-Blog/public/home.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang chủ</title>
+	<link rel="stylesheet" type="text/css" href="../assets/home-style.css">
+</head>
+<body>
+	<div>
+		<div class="wrapper">
+			<header>
+				<div class="top-header">
+					<nav>
+						<ul>
+							<div>
+								<li><a href="">Mới nhất</a></li>
+							</div>
+							<div>
+								<li><a href="">Điều hướng</a></li>
+								<ul>
+									<li><a href="">Danh mục</a></li>
+									<li><a href="">Danh mục</a></li>
+									<li><a href="">Danh mục</a></li>
+									<li><a href="">Danh mục</a></li>
+								</ul>
+							</div>
+						</ul>
+					</nav>
+					<div class="logo">
+						<a href=""><img src="" alt="Logo"></a>
+					</div>
+					<div class="social">
+						<ul>
+							<div>
+								<li><a href="">Mới nhất</a></li>
+							</div>
+							<div>
+								<li><a href="">Mới nhất</a></li>
+							</div>
+							<div>
+								<li><a href="">Mới nhất</a></li>
+							</div>
+						</ul>
+					</div>
+				</div>
+				<div class="introduction">
+					<div class="slider">
+						<div class="left-arrow">
+							<a href=""><img src="" alt="Mũi tên sang trái"></a>
+						</div>
+						<div>
+							<div>
+								<div class="intro-title">
+									<a href="">Giới thiệu về blog</a>
+								</div>
+								<div class="intro-img">
+									<a href=""><img src="" alt="Ảnh bìa giới thiệu"></a>
+								</div>
+							</div>
+						</div>
+						<div class="right-arrow">
+							<a href=""><img src="" alt="Mũi tên sang phải"></a>
+						</div>
+					</div>
+				</div>
+			</header>
+			<div class="content">
+				<section class="main-content">
+					<div class="post-link">
+						<div class="post-thumb">
+							<a href=""><img src="" alt="Bài viết"></a>
+						</div>
+						<div class="post-txt">
+							<div class="post-title">
+								<a href="">Tiêu đề bài viết</a>
+							</div>
+							<div class="post-intro">
+								<p>Lời dẫn cho bài viết</p>
+							</div>
+						</div>
+					</div>
+					<div class="post-link">
+						<div class="post-thumb">
+							<a href=""><img src="" alt="Bài viết"></a>
+						</div>
+						<div class="post-txt">
+							<div class="post-title">
+								<a href="">Tiêu đề bài viết</a>
+							</div>
+							<div class="post-intro">
+								<p>Lời dẫn cho bài viết</p>
+							</div>
+						</div>
+					</div>
+					<div class="pre-post-link">
+						<div class="pre-post-thumb">
+							<a href=""><img src="" alt="Bài viết"></a>
+						</div>
+						<div class="pre-post-txt">
+							<div class="pre-post-title">
+								<a href="">Tiêu đề bài viết</a>
+							</div>
+							<div class="pre-post-intro">
+								<p>Lời dẫn cho bài viết</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<aside class="side-content"> <!-- Show các bài viết nổi bật -->
+					<div class="">
+						<p>Các bài viết nổi bật</p>
+					</div>
+					<div class="small-pre-post-link">
+						<div class="small-pre-post-thumb">
+							<a href=""><img src="" alt="Bài viết"></a>
+						</div>
+						<div class="small-pre-post-txt">
+							<div class="small-pre-post-title">
+								<a href="">Tiêu đề bài viết</a>
+							</div>
+							<div class="view-counter">
+								<img src="" alt="Số lượt xem bài viết"> <!-- Ảnh con mắt -->
+								<p>123</p> <!-- Hiện số lượt xem bài viết, sửa lại bằng JS -->
+							</div>
+						</div>
+					</div>
+				</aside>
+			</div>
+			<footer>
+				<div class="subscribe">
+					<div>
+						<p>Đăng ký nhận tin</p>
+						<p>Bạn sẽ nhận được email cho mỗi 10 bài viết mới</p>
+					</div>
+					<div>
+						<form method="post" action="">
+							<input type="text" name="fname" class="input" placeholder="Họ và tên">
+							<input type="email" name="mail" class="input" placeholder="Email">
+							<button type="submit" name="submit">Đăng ký</button>
+						</form>
+					</div>
+				</div>
+				<div class="infomation">
+					<div class="copy">
+						<p>Blog 2021</p>
+						<p>Bản quyền thuộc về .</p>
+					</div>
+					<div class="category">
+						<div class="info-cate">
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+						</div>
+						<div class="post-cate">
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+							<div>
+								<a href="">Danh mục</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
